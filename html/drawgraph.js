d1ddbc5d8ea2b13e186899f5bf6f81a120cb760a
Johannes Filter <hi@jfilter.de>
2016-06-21 14:39:33 +0200

    draw real simulation results & dummy histogram

--- html/drawgraph.js
+++ html/drawgraph.js
@@ -146,10 +146,96 @@ class SimulationResultChart extends LineChart {
   }
 }
 
+function histogramChart() {
+  var margin = {top: 0, right: 0, bottom: 20, left: 0},
+      width = 960,
+      height = 500;
+
+  var histogram = d3.layout.histogram(),
+      x = d3.scale.ordinal(),
+      y = d3.scale.linear(),
+      xAxis = d3.svg.axis().scale(x).orient("bottom").tickSize(6, 0);
+
+  function chart(selection) {
+    selection.each(function(data) {
+
+      // Compute the histogram.
+      data = histogram(data);
+
+      // Update the x-scale.
+      x   .domain(data.map(function(d) { return d.x; }))
+          .rangeRoundBands([0, width - margin.left - margin.right], .1);
+
+      // Update the y-scale.
+      y   .domain([0, d3.max(data, function(d) { return d.y; })])
+          .range([height - margin.top - margin.bottom, 0]);
+
+      // Select the svg element, if it exists.
+      var svg = d3.select(this).selectAll("svg").data([data]);
+
+      // Otherwise, create the skeletal chart.
+      var gEnter = svg.enter().append("svg").append("g");
+      gEnter.append("g").attr("class", "bars");
+      gEnter.append("g").attr("class", "x axis");
+
+      // Update the outer dimensions.
+      svg .attr("width", width)
+          .attr("height", height);
+
+      // Update the inner dimensions.
+      var g = svg.select("g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      // Update the bars.
+      var bar = svg.select(".bars").selectAll(".bar").data(data);
+      bar.enter().append("rect");
+      bar.exit().remove();
+      bar .attr("width", x.rangeBand())
+          .attr("x", function(d) { return x(d.x); })
+          .attr("y", function(d) { return y(d.y); })
+          .attr("height", function(d) { return y.range()[0] - y(d.y); })
+          .order();
+
+      // Update the x-axis.
+      g.select(".x.axis")
+          .attr("transform", "translate(0," + y.range()[0] + ")")
+          .call(xAxis);
+    });
+  }
+
+  chart.margin = function(_) {
+    if (!arguments.length) return margin;
+    margin = _;
+    return chart;
+  };
+
+  chart.width = function(_) {
+    if (!arguments.length) return width;
+    width = _;
+    return chart;
+  };
+
+  chart.height = function(_) {
+    if (!arguments.length) return height;
+    height = _;
+    return chart;
+  };
+
+  // Expose the histogram's value, range and bins method.
+  d3.rebind(chart, histogram, "value", "range", "bins");
+
+  // Expose the x-axis' tickFormat method.
+  d3.rebind(chart, xAxis, "tickFormat");
+
+  return chart;
+}
+
+
 $(document).ready(function(){
   const T = 100;
   const N = 20;
   const maxPrice = 20;
+  const counts = 12;
 
   function randomArray(length) {
   return Array.apply(null, Array(length)).map(function(_, i) {
@@ -165,28 +251,59 @@ $(document).ready(function(){
       pricingPolicyChart = new PricingPolicyChart(400, T, N, maxPrice, d3.select('#pricingpolicy'));
       result.forEach(row => pricingPolicyChart.drawLine(row.prices, row.n));
 
-      sims = []
-      for(let i = 0; i < 12; i++) {
-        sims.push(randomArray(T));
-      }
-      sims.forEach( s => {
-        const newDiv = $('<div></div>')
-          .addClass('col-md-3')
-          .addClass('text-center')
-        $('#sim').append(newDiv);
+      fetch('/api/simulations', {
+        method: 'POST',
+        body: JSON.stringify({ T, N, counts }),
+        headers: { 'Content-Type': 'application/json' },
+      }).then(res => res.json())
+        .then(result => {
 
-        chart = new SimulationResultChart(200, T, N, maxPrice, d3.select(newDiv.get()[0]))
-          .drawLine(s);
+        profits = []
+        sims = []
+        result.forEach( row => {
+          profits.push(row.profit);
 
-        const newB = $('<button></button')
-          .text('Details')
-          .addClass('btn')
-          .addClass('btn-default');
+          const newDiv = $('<div></div>')
+            .addClass('col-md-3')
+            .addClass('text-center')
+          $('#sim').append(newDiv);
 
-        newDiv.append(newB);
+          chart = new SimulationResultChart(200, T, N, maxPrice, d3.select(newDiv.get()[0]))
+            .drawLine(row.self);
 
-      });
+          const newLabel = $('<div></div>')
+            .html(Math.round(row.profit))
+            .addClass('label')
+            .addClass('label-default');
+          newDiv.append(newLabel);
 
+          const newB = $('<button></button')
+            .text('Details')
+            .addClass('btn')
+            .addClass('btn-default')
+            .addClass('btn-xs');
+          newDiv.append(newB);
 
-    });
+        });
+
+        function irwinHallDistribution(n, m) {
+          var distribution = [];
+          for (var i = 0; i < n; i++) {
+            for (var s = 0, j = 0; j < m; j++) {
+              s += Math.random();
+            }
+            distribution.push(s / m);
+          }
+          return distribution;
+        }
+
+        d3.select("#histogram")
+            .datum(irwinHallDistribution(10000, 10))
+            // .datum(profits)
+          .call(histogramChart()
+            .bins(d3.scale.linear().ticks(20))
+            .tickFormat(d3.format(".02f")));
+      })
+  })
+
 });
